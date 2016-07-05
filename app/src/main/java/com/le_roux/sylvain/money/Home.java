106e7c39f92206e445c3976ca41e2101c9ccc07b
Sylvain LE ROUX <sylvain.leroux@telecom-paristech.fr>
2016-07-05 14:30:45 +0200

    Initial commit

---
+++ app/src/main/java/com/le_roux/sylvain/money/Home.java
@@ -0,0 +1,13 @@
+package com.le_roux.sylvain.money;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
